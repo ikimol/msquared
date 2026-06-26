024fb31617c3ebd6c0b26d1f39a6512a09d33dda
Miklos Molnar <miklos.molnar@me.com>
2026-06-27 01:20:00 +0200

    Add `Angle` literals and add `constexpr`

--- core/include/msquared/core/math/angle.hpp
+++ core/include/msquared/core/math/angle.hpp
@@ -7,18 +7,18 @@
 namespace msq {
 
 class Angle {
-    friend Angle degrees(float);
-    friend Angle radians(float);
+    friend constexpr Angle degrees(float);
+    friend constexpr Angle radians(float);
 
 public:
     /// Default constructor
     constexpr Angle() noexcept = default;
 
     /// Get the value in degrees
-    float as_degrees() const { return m_degrees; }
+    constexpr float as_degrees() const { return m_degrees; }
 
     /// Get the value in radians
-    float as_radians() const { return m_degrees * pi / 180.f; }
+    constexpr float as_radians() const { return m_degrees * pi / 180.f; }
 
 private:
     // private value constructor
@@ -32,83 +32,104 @@ private:
 // factory functions
 
 /// Create an angle from a value in degrees
-inline Angle degrees(float deg) {
+constexpr Angle degrees(float deg) {
     return Angle(deg);
 }
 
 /// Create an angle from a value in radians
-inline Angle radians(float rad) {
+constexpr Angle radians(float rad) {
     return Angle(rad * 180.f / pi);
 }
 
 // operators
 
-inline bool operator==(const Angle& lhs, const Angle& rhs) {
+constexpr bool operator==(const Angle& lhs, const Angle& rhs) {
     return lhs.as_degrees() == rhs.as_degrees();
 }
 
-inline bool operator!=(const Angle& lhs, const Angle& rhs) {
+constexpr bool operator!=(const Angle& lhs, const Angle& rhs) {
     return lhs.as_degrees() != rhs.as_degrees();
 }
 
-inline bool operator<(const Angle& lhs, const Angle& rhs) {
+constexpr bool operator<(const Angle& lhs, const Angle& rhs) {
     return lhs.as_degrees() < rhs.as_degrees();
 }
 
-inline bool operator<=(const Angle& lhs, const Angle& rhs) {
+constexpr bool operator<=(const Angle& lhs, const Angle& rhs) {
     return lhs.as_degrees() <= rhs.as_degrees();
 }
 
-inline bool operator>(const Angle& lhs, const Angle& rhs) {
+constexpr bool operator>(const Angle& lhs, const Angle& rhs) {
     return lhs.as_degrees() > rhs.as_degrees();
 }
 
-inline bool operator>=(const Angle& lhs, const Angle& rhs) {
+constexpr bool operator>=(const Angle& lhs, const Angle& rhs) {
     return lhs.as_degrees() >= rhs.as_degrees();
 }
 
-inline Angle operator-(const Angle& angle) {
+constexpr Angle operator-(const Angle& angle) {
     return degrees(-angle.as_degrees());
 }
 
-inline Angle operator+(const Angle& lhs, const Angle& rhs) {
+constexpr Angle operator+(const Angle& lhs, const Angle& rhs) {
     return degrees(lhs.as_degrees() + rhs.as_degrees());
 }
 
-inline Angle& operator+=(Angle& lhs, const Angle& rhs) {
+constexpr Angle& operator+=(Angle& lhs, const Angle& rhs) {
     lhs = lhs + rhs;
     return lhs;
 }
 
-inline Angle operator-(const Angle& lhs, const Angle& rhs) {
+constexpr Angle operator-(const Angle& lhs, const Angle& rhs) {
     return degrees(lhs.as_degrees() - rhs.as_degrees());
 }
 
-inline Angle& operator-=(Angle& lhs, const Angle& rhs) {
+constexpr Angle& operator-=(Angle& lhs, const Angle& rhs) {
     lhs = lhs - rhs;
     return lhs;
 }
 
-inline Angle operator*(const Angle& lhs, float rhs) {
+constexpr Angle operator*(const Angle& lhs, float rhs) {
     return degrees(lhs.as_degrees() * rhs);
 }
 
-inline Angle operator*(float lhs, const Angle& rhs) {
+constexpr Angle operator*(float lhs, const Angle& rhs) {
     return rhs * lhs;
 }
 
-inline Angle& operator*=(Angle& lhs, float rhs) {
+constexpr Angle& operator*=(Angle& lhs, float rhs) {
     lhs = lhs * rhs;
     return lhs;
 }
 
-inline Angle operator/(const Angle& lhs, float rhs) {
+constexpr Angle operator/(const Angle& lhs, float rhs) {
     return degrees(lhs.as_degrees() / rhs);
 }
 
-inline Angle& operator/=(Angle& lhs, float rhs) {
+constexpr Angle& operator/=(Angle& lhs, float rhs) {
     lhs = lhs / rhs;
     return lhs;
 }
 
+// literals
+
+namespace literals {
+
+constexpr Angle operator""_deg(long double angle) {
+    return degrees(static_cast<float>(angle));
+}
+
+constexpr Angle operator""_deg(unsigned long long angle) {
+    return degrees(static_cast<float>(angle));
+}
+
+constexpr Angle operator""_rad(long double angle) {
+    return radians(static_cast<float>(angle));
+}
+
+constexpr Angle operator""_rad(unsigned long long angle) {
+    return radians(static_cast<float>(angle));
+}
+
+} // namespace literals
 } // namespace msq
